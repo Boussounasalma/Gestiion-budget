1691ef3ecfb2f3db4ce8f8d91a30623dda56d603
Salma Boussouna <salmaboussouna84@gmail.com>
2023-12-25 14:42:44 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { WalletComponent } from './wallet/wallet.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
+const routes: Routes = [
+{path:'',component: AuthentificationComponent},
+{path:'Register',component: RegisterComponent},
+{path:'dashboard',component: DashboardComponent},
+{path:'transaction', component: TransactionComponent},
+{path:'categories',component:CategorieComponent},
+{path:'wallet',component:WalletComponent},
+{path:'statistique',component:StatistiqueComponent},
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; // Importez le module HttpClientModule
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { RegisterComponent } from './register/register.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { WalletComponent } from './wallet/wallet.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { StatistiqueComponent } from './statistique/statistique.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AuthentificationComponent,
+    RegisterComponent,
+    CategorieComponent,
+    TransactionComponent,
+    WalletComponent,
+    StatistiqueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CanvasJSAngularChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
